72275693495465a248686b836e88391646cbca75
Brian <clockworkai@outlook.com>
2026-01-02 21:09:49 +0000

    refactor: remove flatten utility functions and their tests

--- server/utils/flatten.test.js
+++
@@ -1,9 +0,0 @@
-const flattenObject = require('./flatten')
-
-describe('test flattenObject', () => {
-
-    test('should flatten an Object', () => {
-      const flat = flattenObject({a:{b:1}})
-      expect(flat).toEqual({"a.b":1});
-    })
-})
